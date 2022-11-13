f19ba23da38ab522ab9cacf739ec09b35479037f
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2022-11-12 22:37:16 -0500

    Lots of big updates

--- MS Band WebTile Generator/BuilderPages/DataBuilder.xaml.cs
+++ MS Band WebTile Generator/BuilderPages/DataBuilder.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -10,9 +11,15 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
+using System.ServiceModel;
 using Windows.UI.Xaml.Media;
+using Windows.UI;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
+using System.ServiceModel.Syndication;
+using System.Net.Http;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -36,6 +43,47 @@ namespace MS_Band_WebTile_Generator.BuilderPages
         private void DataNext_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(MS_Band_WebTile_Generator.BuilderPages.StyleBuilder), null, new SlideNavigationTransitionInfo() { Effect = SlideNavigationTransitionEffect.FromRight });
+        }
+
+        public static string rsslink;
+        private void DataFeedBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            rsslink = DataFeedBox.Text;
+            ParseRSS();
+        }
+
+        public void ParseRSS()
+        {
+            // Set defualt
+            SyndicationFeed feed = null;
+
+            // Try and read RSS/ATOM Feed
+            try
+            {
+                using (var reader = XmlReader.Create(rsslink))
+                {
+                    feed = SyndicationFeed.Load(reader);
+                }
+            }
+            // Handle bad resource
+            catch { 
+                feed = null;
+                DataStatus.Text = "Data feed unavailable, please try again later.";
+                DataStatus.Foreground = new SolidColorBrush(Colors.Red);
+            }
+
+            if (feed != null)
+            {
+                // Push parsed data
+                foreach (var element in feed.Items.Take(1))
+                {
+                    DataStatus.Text = "Data feed found.";
+                    DataStatus.Foreground = new SolidColorBrush(Colors.LightGreen);
+                    RSSTitle.Text = ($"Title: {element.Title.Text}");
+                    RSSDesc.Text = ($"Description: {element.Summary.Text}");
+                    RSSDate.Text = ($"Date and Time Published: {element.PublishDate}");
+                }
+            }
         }
     }
 }
--- MS Band WebTile Generator/BuilderPages/StyleBuilder.xaml.cs
+++ MS Band WebTile Generator/BuilderPages/StyleBuilder.xaml.cs
@@ -14,6 +14,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using System.Xml.Linq;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -33,6 +35,7 @@ namespace MS_Band_WebTile_Generator.BuilderPages
         public static string DescriptionTile;
         public static string AuthorTile;
         public static string OrgTile;
+        public static int RefreshInt;
 
         private void StyleBack_Click(object sender, RoutedEventArgs e)
         {
@@ -42,6 +45,153 @@ namespace MS_Band_WebTile_Generator.BuilderPages
         private void StyleNext_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(MS_Band_WebTile_Generator.BuilderPages.CompleteBuilder), null, new SlideNavigationTransitionInfo() { Effect = SlideNavigationTransitionEffect.FromRight });
+            var obj = new
+            {
+                manifestVersion = 1,
+                name = "SEA & JFK weather",
+                description = "This tile shows the current weather at SEA and JFK",
+                version = 1,
+                versionString = "1",
+                author = "Microsoft Band Team",
+                organization = "MS Health",
+                contactEmail = "healthms@microsoft.com",
+                tileIcon = new Dictionary<int, string>
+                {
+                    [46] = "icons/tileIcon.png"
+                },
+                badgeIcon = new Dictionary<int, string>
+                {
+                    [24] = "icons/badgeIcon.png",
+                },
+                icons = new
+                {
+                    SEAIcon = "icons/SEA.png",
+                    JFKIcon = "icons/JFK.png",
+                },
+                refreshIntervalMinutes = 30,
+                resources = new dynamic[] {
+                new {
+                    url = "http://services.faa.gov/airport/status/SEA?format=json",
+                    style = "Simple",
+                    content = new
+                    {
+                        SEAWeather = "weather.weather",
+                        SEAvisibility = "weather.visibility"
+                    },
+                },
+               new {
+                    url = "http://services.faa.gov/airport/status/JFK?format=json",
+                    style = "Simple",
+                    content = new
+                    {
+                        JFKWeather = "weather.weather",
+                        JFKvisibility = "weather.visibility"
+                    },
+                },
+            },
+                pages = new dynamic[]
+    {
+                new {
+                    layout = "MSBand_NoScrollingText",
+                    condition = "true",
+                    textBindings = new []
+                    {
+                        new
+                        {
+                            elementId = "1",
+                            value = "Airport Weather",
+                        },
+                        new
+                        {
+                            elementId = "2",
+                            value = "SEA: {{SEAweather}}",
+                        },
+                        new
+                        {
+                            elementId = "3",
+                            value = "JFK: {{JFKweather}}",
+                        },
+                    }
+                },
+                new {
+                    layout = "MSBand_SingleMetricWithSecondary",
+                    condition = "true",
+                    iconBindings = new []
+                    {
+                        new
+                        {
+                            elementId = "21",
+                            conditions = new[]
+                            {
+                                new
+                                {
+                                    condition = "true",
+                                    icon = "SEAIcon",
+                                },
+                            },
+                        },
+                    },
+                    textBindings = new []
+                    {
+                        new
+                        {
+                            elementId = "11",
+                            value = "SEA",
+                        },
+                        new
+                        {
+                            elementId = "12",
+                            value = "Visibility",
+                        },
+                        new
+                        {
+                            elementId = "22",
+                            value = "{{SEAvisibility}}",
+                        },
+                    },
+                },
+                  new {
+                    layout = "MSBand_SingleMetricWithSecondary",
+                    condition = "true",
+                    iconBindings = new []
+                    {
+                        new
+                        {
+                            elementId = "21",
+                            conditions = new[]
+                            {
+                                new
+                                {
+                                    condition = "true",
+                                    icon = "JFKIcon",
+                                },
+                            },
+                        },
+                    },
+                    textBindings = new []
+                    {
+                        new
+                        {
+                            elementId = "11",
+                            value = "JFK",
+                        },
+                        new
+                        {
+                            elementId = "12",
+                            value = "Visibility",
+                        },
+                        new
+                        {
+                            elementId = "22",
+                            value = "{{JFKvisibility}}",
+                        },
+                    },
+                  },
+                },
+            };
+
+            var json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            Console.WriteLine(json);
         }
 
         private void TitleTextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -74,7 +224,7 @@ namespace MS_Band_WebTile_Generator.BuilderPages
             Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
             if (file != null)
             {
-                // Application now has read/write access to the picked file
+                // Fix later
             }
             else
             {
@@ -102,7 +252,20 @@ namespace MS_Band_WebTile_Generator.BuilderPages
 
         private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            StorageFolder newFolder = await DownloadsFolder.CreateFolderAsync("WebTile");
+            try
+            {
+                StorageFolder newFolder = await DownloadsFolder.CreateFolderAsync("WebTile", CreationCollisionOption.FailIfExists);
+                StorageFolder subFolder = await DownloadsFolder.CreateFolderAsync("WebTile\\icons", CreationCollisionOption.FailIfExists);
+            }
+            catch
+            {
+                ExceptionFileAccess.IsOpen = true;
+            }
+        }
+
+        private void RefreshSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            RefreshInt = Convert.ToInt32(e.NewValue);
         }
     }
 }
